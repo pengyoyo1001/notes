ccf57be9a92ff6d27b6f5737097ec8b8962d212a
pengyoyo <hb1176497344@gmail.com>
2023-04-05 18:51:50 +0800

    b c file

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
